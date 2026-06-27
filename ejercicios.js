52f9dcc77c085d6e012e88c0c8783fe088fa6dce
johannbanegas <johannn.banegas@gmail.com>
2026-06-27 03:12:07 -0400

    Ejercicios de varialbles y operadores de JavaScript.

---
+++ ejercicios.js
@@ -0,0 +1,82 @@
+//Presenración
+
+let nombre = "Johann";
+let edad = 17;
+
+console.log("Hola, me llamo %s y tengo %d años.", nombre, edad);
+
+//Suma de dos numeros
+
+let numero1 = 13;
+let numero2 = 10;
+
+console.log(numero1 + numero2);
+
+//Area de un rectangulo
+
+let base = 10;
+let altura = 6;
+
+let area = base * altura;
+
+console.log(area);
+
+//¿Es mayor de edad?
+
+let edad1 = 25;
+
+let esMayorDeEdad = edad1 >= 18;
+
+console.log(esMayorDeEdad);
+
+//Comparación estricta
+
+let comparacion = 50 === "50";
+
+console.log(comparacion);
+
+//Numero par o impar
+
+let numero = 14;
+
+console.log(numero % 2 == 0);
+
+//Estado de una lampara
+
+let encendida = true;
+
+console.log(encendida);
+console.log(!encendida);
+
+//Acceso permitido
+
+let edad2 = 20;
+let tieneEntrada = true;
+
+let puedeEntrar = edad2 >= 18 && tieneEntrada;
+
+console.log(puedeEntrar);
+
+//Usuario sin nombre
+
+let usuario;
+
+console.log(usuario);
+console.log(typeof usuario);
+
+//Compra con descuento
+
+let producto = 200;
+let productoConDescuento = producto * 0.65;
+
+console.log(productoConDescuento);
+
+let a = 12;
+let b = "12";
+let c = true;
+
+console.log(a == b);
+console.log(a === b);
+console.log(c && a > 10);
+console.log(!c);
+console.log(a % 5);
---
+++ main.js
@@ -0,0 +1,80 @@
+//Variables
+
+let nombre = "Juan";
+let edad = 20;
+
+edad = 21;
+
+console.log(nombre);
+console.log(edad);
+
+const pais = "Bolivia";
+
+console.log(pais);
+
+//Tipos de datos primitivos
+
+let mensaje = "Hola mundo";
+
+console.log(typeof mensaje);
+
+let precio = 19.99;
+
+console.log(typeof precio);
+
+let esMayor = true;
+let tienePermiso = false;
+
+console.log(typeof esMayor);
+
+let apellido;
+
+console.log(typeof apellido);
+
+let usuario = null;
+console.log(typeof usuario);
+
+//Operadores aritmeticos
+
+let resultado = 10 + 5; //Suma
+console.log(resultado);
+
+resultado = 10 - 5; //Resta
+console.log(resultado);
+
+resultado = 10 / 5; //Division
+console.log(resultado);
+
+resultado = 10 % 5; //Modulo
+console.log(resultado);
+
+//Operadpres de comparacion
+
+console.log(5 == "5"); //igual debil
+console.log(5 === "5"); //Igual estricta
+console.log(5 != 3); //Diferente
+console.log(5 !== "5"); //Diferente estricto
+
+console.log(10 > 5); //mayorque
+console.log(10 < 5); //menorque
+console.log(10 >= 10); //mayoroigual
+console.log(10 <+ 7); //menoroigual
+
+//Operadores Logicos
+
+edad = 20;
+let tieneCarnet = true;
+
+console.log(edad >= 19 && tieneCarnet); //AND
+
+let esAdmin = false;
+let esModerador = true;
+
+console.log(esAdmin || esModerador); //OR
+
+let activo = true;
+
+console.log(!activo); //NOT
+
+
+
